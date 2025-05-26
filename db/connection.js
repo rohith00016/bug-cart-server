7be7c2b774b2a4dc4e633c4482f3a677a58ab70e
rohith00016 <rohithm.22mca@kongu.edu>
2025-05-26 13:01:02 +0530

    mongo url

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const seedDatabase = require("./seed");
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ecommerce");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB is connected");
     seedDatabase();
   } catch (error) {
