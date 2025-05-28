3faaf19c0571a80ec1e4d295adbd8d6cea4c506f
rohith00016 <rohithm.22mca@kongu.edu>
2025-05-28 11:18:11 +0530

    order model fields updated

--- model/order.js
+++ model/order.js
@@ -33,6 +33,7 @@ const orderSchema = new mongoose.Schema(
     },
     shippingAddress: {
       address: { type: String, required: true },
+      street: { type: String, required: true },
       state: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
